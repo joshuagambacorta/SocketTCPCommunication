b9266eefe0480181e2f16552576d2432e7585524
joshua <joshua.gambacorta@gmail.com>
2022-04-21 20:09:18 +0200

    First commit

---
+++ src/joshua/good/student/ManageClient.java
@@ -0,0 +1,11 @@
+package joshua.good.student;
+
+public class ManageClient {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.comunica();
+        client.chiudi();
+    }
+
+}
---
+++ src/joshua/good/student/ManageServer.java
@@ -0,0 +1,11 @@
+package joshua.good.student;
+
+public class ManageServer {
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.comunica();
+        server.chiudi();
+    }
+
+}
